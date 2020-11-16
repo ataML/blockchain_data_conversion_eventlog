147c7886124f3eaed9056eb7952c048f5be53587
ataML <ata.mazloomian@gmail.com>
2020-11-16 16:09:07 +0100

    Update convert_json.sh

--- scripts/fabric/convert_json.sh
+++ scripts/fabric/convert_json.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-for ((c=0; c<132; c++))
+num_blocks=135
+for ((c=0; c<num_blocks; c++))
 do 
     configtxlator proto_decode --input block$c.block --type common.Block --output /Users/amazloomian/Desktop/guided\ research/data/$c.json
 done
